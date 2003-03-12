4fde52aa957a490ddf91aa4a352999f1997e357f
j_ali <>
2003-03-12 11:15:18 +0000

    Merge changes from NetHack 3.4.1

--- sys/winnt/winnt.c
+++ sys/winnt/winnt.c
@@ -1,4 +1,4 @@
-/*	SCCS Id: @(#)winnt.c	 3.4	 1997/04/12		  */
+/*	SCCS Id: @(#)winnt.c	 3.4	 $Date$		  */
 /* Copyright (c) NetHack PC Development Team 1993, 1994 */
 /* NetHack may be freely redistributed.  See license for details. */
 
