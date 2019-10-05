7435c46399fce5ba5bdcef17ce055ce136551ffe
Elronnd <elronnd@elronnd.net>
2019-10-04 23:40:44 -0700

    Complete build with build.sh; woo!

--- build.sh
+++ build.sh
@@ -22,3 +22,100 @@ done
 wait
 
 cc -o slashem *.o -lncursesw
+
+spec_levs() {
+	bison -d ../util/lev_comp.y
+	mv lev_comp.tab.c lev_yacc.c
+	mv lev_comp.tab.h include/lev_comp.h
+
+	flex ../util/lev_comp.l
+	mv lex.yy.c lev_lex.c
+
+	cc lev_yacc.c lev_lex.c ../util/lev_main.c ../src/alloc.c ../util/panic.c ../src/drawing.c ../src/decl.c ../src/objects.c ../src/monst.c -o lev_comp -I../include -Iinclude
+
+
+	bison -d ../util/dgn_comp.y
+	mv dgn_comp.tab.c dgn_yacc.c
+	mv dgn_comp.tab.h include/dgn_comp.h
+
+	flex ../util/dgn_comp.l
+	mv lex.yy.c dgn_lex.c
+
+	cc dgn_yacc.c dgn_lex.c ../util/dgn_main.c ../src/alloc.c ../util/panic.c -o dgn_comp -I../include -Iinclude
+
+	./lev_comp ../dat/beholder.des
+	./lev_comp ../dat/bigroom.des
+	./lev_comp ../dat/blkmar.des
+	./lev_comp ../dat/castle.des
+	./lev_comp ../dat/grund.des
+	./lev_comp ../dat/dragons.des
+	./lev_comp ../dat/endgame.des
+	./lev_comp ../dat/frnknstn.des
+	./lev_comp ../dat/gehennom.des
+	./lev_comp ../dat/giants.des
+	./lev_comp ../dat/guild.des
+	./lev_comp ../dat/knox.des
+	./lev_comp ../dat/kobold-1.des
+	./lev_comp ../dat/kobold-2.des
+	./lev_comp ../dat/lich.des
+	./lev_comp ../dat/mall-1.des
+	./lev_comp ../dat/mall-2.des
+	./lev_comp ../dat/medusa.des
+	./lev_comp ../dat/mines.des
+	./lev_comp ../dat/mtemple.des
+	./lev_comp ../dat/newmall.des
+	./lev_comp ../dat/nightmar.des
+	./lev_comp ../dat/nymph.des
+	./lev_comp ../dat/oracle.des
+	./lev_comp ../dat/rats.des
+	./lev_comp ../dat/sea.des
+	./lev_comp ../dat/sokoban.des
+	./lev_comp ../dat/spiders.des
+	./lev_comp ../dat/stor-1.des
+	./lev_comp ../dat/stor-2.des
+	./lev_comp ../dat/stor-3.des
+	./lev_comp ../dat/tomb.des
+	./lev_comp ../dat/tower.des
+	./lev_comp ../dat/yendor.des
+
+	./lev_comp ../dat/Arch.des
+	./lev_comp ../dat/Barb.des
+	./lev_comp ../dat/Caveman.des
+	./lev_comp ../dat/Flame.des
+	./lev_comp ../dat/Healer.des
+	./lev_comp ../dat/Ice.des
+	./lev_comp ../dat/Knight.des
+	./lev_comp ../dat/Monk.des
+	./lev_comp ../dat/Necro.des
+	./lev_comp ../dat/Priest.des
+	./lev_comp ../dat/Ranger.des
+	./lev_comp ../dat/Rogue.des
+	./lev_comp ../dat/Samurai.des
+	./lev_comp ../dat/Tourist.des
+	./lev_comp ../dat/Slayer.des
+	./lev_comp ../dat/Valkyrie.des
+	./lev_comp ../dat/Wizard.des
+	./lev_comp ../dat/Yeoman.des
+
+
+	./dgn_comp dat/dungeon.pdf
+}
+
+spec_levs
+
+mkdir dater
+cp ../dat/help ../dat/hh ../dat/cmdhelp ../dat/history ../dat/opthelp ../dat/wizhelp ../dat/gypsy.txt dat/data dat/options dat/oracles dat/quest.dat dat/rumors dater/
+cd dater
+../dlb cf nhshare *
+mv nhshare ..
+cd ..
+rm -rf dater
+
+cp dat/dungeon .
+./dlb cf nhushare dungeon *.lev
+
+
+touch $install_dir/perm
+touch $install_dir/logfile
+touch $install_dir/xlogfile
+mkdir $install_dir/save
