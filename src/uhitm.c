moon-chilled/slashem9

commit 2d67a7c0c67f6bea71558e8cf188fcaff865279a

Fix all warnings clang emits under default settings except for the k&r prototypes one This actually fixed quite a few bugs Also, You_hear() no longer works with formatted strings; you need You_hearf() for that. Lots of other minor architectural changes too

src/uhitm.c
@@ -493,11 +493,12 @@ struct attack *uattk;
 	if (override_confirmation) {
 	    /* this may need to be generalized if weapons other than
 	       Stormbringer acquire similar anti-social behavior... */
-	    if (flags.verbose)
-		if (override_confirmation == HIT_UWEP)
-		    Your("bloodthirsty blade attacks!");
-		else
-		    pline("The black blade will not be thwarted!");
+		if (flags.verbose) {
+			if (override_confirmation == HIT_UWEP)
+				Your("bloodthirsty blade attacks!");
+			else
+				pline("The black blade will not be thwarted!");
+		}
 	}
 
 	if(!*mhit) {
@@ -733,7 +734,7 @@ int thrown;
 	else if (thrown == 2) launcher = uswapwep;
 	else launcher = 0;
 
-	objenchant = !thrown && no_obj || obj->spe < 0 ? 0 : obj->spe;
+	objenchant = !thrown && obj && obj->spe < 0 ? 0 : obj->spe;
 
 	if (need_one(mon))    canhitmon = 1;
 	if (need_two(mon))    canhitmon = 2;
@@ -2711,9 +2712,9 @@ use_weapon:
 			/* WAC if attacking cockatrice/etc, player is smart
 			   if wielding a weapon.  So don't let him
 			   touch the monster */
-			if ((uwep || u.twoweap && uswapwep) &&
-				(mhit == HIT_UWEP && !uwep ||
-				 mhit == HIT_USWAPWEP && !uswapwep) &&
+			if ((uwep || (u.twoweap && uswapwep)) &&
+				((mhit == HIT_UWEP && !uwep) ||
+				 (mhit == HIT_USWAPWEP && !uswapwep)) &&
 				(touch_petrifies(mon->data) ||
 				 mon->data == &mons[PM_MEDUSA]))
 			    break;
@@ -2756,8 +2757,8 @@ use_weapon:
 			}
 
 			if (dhit && mattk->adtyp == AD_SLEE)
-			    barehanded_hit = (dhit & HIT_UWEP) && !uwep ||
-			      (dhit & HIT_USWAPWEP) && !uswapwep;
+			    barehanded_hit = ((dhit & HIT_UWEP) && !uwep) ||
+			      ((dhit & HIT_USWAPWEP) && !uswapwep);
 
 #if 0 /* Old code */
 			if (uwep) {
@@ -2800,7 +2801,7 @@ use_weapon:
 		case AT_BITE:
 			/* [ALI] Vampires are also smart. They avoid biting
 			   monsters if doing so would be fatal */
-			if ((uwep || u.twoweap && uswapwep) &&
+			if ((uwep || (u.twoweap && uswapwep)) &&
 				is_vampire(youmonst.data) &&
 				(is_rider(mon->data) ||
 				 mon->data == &mons[PM_GREEN_SLIME]))
@@ -2810,7 +2811,7 @@ use_weapon:
 		case AT_BUTT:
 		case AT_TENT:
 			if (i==0 && uwep && (youmonst.data->mlet==S_LICH)) goto use_weapon;
-			if ((uwep || u.twoweap && uswapwep) &&
+			if ((uwep || (u.twoweap && uswapwep)) &&
 				(touch_petrifies(mon->data) ||
 				 mon->data == &mons[PM_MEDUSA]))
 			    break;
@@ -3046,8 +3047,8 @@ uchar aatyp;
 	    if (mhit) {		/* successful attack */
 		long protector = attk_protection((int)aatyp);
 		boolean barehanded = mhit & HIT_BODY ||
-			mhit & HIT_UWEP && !uwep ||
-			mhit & HIT_USWAPWEP && !uswapwep;
+			(mhit & HIT_UWEP && !uwep) ||
+			(mhit & HIT_USWAPWEP && !uswapwep);
 
 		/* hero using monsters' AT_MAGC attack is hitting hand to
 		   hand rather than casting a spell */

Tuprules.tup
@@ -2,4 +2,4 @@ CFLAGS += -Iinclude -Ibuild/include -DDLB -DWIZARD=\"`whoami`\"
 CFLAGS += -g -Werror -O0
 CFLAGS += -std=c99 -D_XOPEN_SOURCE=500 -D_DEFAULT_SOURCE
 CC = cc
-#CC = clang -Wno-knr-promoted-parameter -Wno-format-security -Wno-logical-op-parentheses -Wno-dangling-else
+#CC = clang -Wno-knr-promoted-parameter

include/extern.h
@@ -1513,6 +1513,12 @@ extern boolean is_autopickup_exception(struct obj *, boolean);
 
 /* ### pline.c ### */
 
+// for safety; if you say pline(x), where x is untrusted user data, then the
+// controller of that data has arbitrary code execution.  Maybe.
+#define plines(str) pline("%s", (str))
+#define Noreps(str) Norep("%s", (str))
+#define verbalizes(str) verbalize("%s", (str))
+#define You_hear(str) You_hearf("%s", (str))
 extern void msgpline_add(int, char *);
 extern void msgpline_free(void);
 extern void pline(const char *,...) PRINTF_F(1,2);
@@ -1522,7 +1528,7 @@ extern void You(const char *,...) PRINTF_F(1,2);
 extern void Your(const char *,...) PRINTF_F(1,2);
 extern void You_feel(const char *,...) PRINTF_F(1,2);
 extern void You_cant(const char *,...) PRINTF_F(1,2);
-extern void You_hear(const char *,...) PRINTF_F(1,2);
+extern void You_hearf(const char *,...) PRINTF_F(1,2);
 extern void pline_The(const char *,...) PRINTF_F(1,2);
 extern void There(const char *,...) PRINTF_F(1,2);
 extern void verbalize(const char *,...) PRINTF_F(1,2);

include/monst.h
@@ -176,8 +176,11 @@ struct monst {
  */
 
 #define newmonst(xl) alloc((unsigned)(xl) + sizeof(struct monst))
-#define dealloc_monst(mon) if ((mon)->isshk) shk_free(mon); else \
-				free((mon))
+#define dealloc_monst(mon) do { if ((mon)->isshk) \
+	shk_free(mon); \
+else \
+	free(mon); \
+} while (0)
 
 /* these are in mspeed */
 #define MSLOW 1		/* slow monster */

src/apply.c
@@ -267,13 +267,13 @@ static int use_stethoscope(struct obj *obj) {
 	lev = &levl[rx][ry];
 	switch(lev->typ) {
 	case SDOOR:
-		You_hear(hollow_str, "door");
+		You_hearf(hollow_str, "door");
 		cvt_sdoor_to_door(lev);		/* ->typ = DOOR */
 		if (Blind) feel_location(rx,ry);
 		else newsym(rx,ry);
 		return res;
 	case SCORR:
-		You_hear(hollow_str, "passage");
+		You_hearf(hollow_str, "passage");
 		lev->typ = CORR;
 		unblock_point(rx,ry);
 		if (Blind) feel_location(rx,ry);

src/artifact.c
@@ -1467,7 +1467,7 @@ static int arti_invoke(struct obj *obj) {
 						 }
 					 }
 					 if (unseen)
-						 You_hear("%s of intense pain!", unseen > 1 ? "cries" : "a cry");
+						 You_hearf("%s of intense pain!", unseen > 1 ? "cries" : "a cry");
 					 break;
 			case DEATH_GAZE:
 					 if (u.uluck < -9) {

src/do.c
@@ -80,7 +80,7 @@ boolean boulder_hits_pool(struct obj *otmp, int rx, int ry, boolean pushing) {
 			      the(xname(otmp)), fills_up? "fills":"falls into",
 			      what);
 		    } else if (flags.soundok)
-			You_hear("a%s splash.", lava ? " sizzling" : "");
+			You_hearf("a%s splash.", lava ? " sizzling" : "");
 		    wake_nearto(rx, ry, 40);
 		}
 
@@ -157,7 +157,7 @@ boolean flooreffects(struct obj *obj, int x, int y, const char *verb) {
 				if ((x == u.ux) && (y == u.uy))
 					You_hear("a CRASH! beneath you.");
 				else
-					You_hear("the boulder %s.", verb);
+					You_hearf("the boulder %s.", verb);
 			} else if (cansee(x, y)) {
 				pline_The("boulder %s%s.",
 				    t->tseen ? "" : "triggers and ",
@@ -196,7 +196,7 @@ boolean flooreffects(struct obj *obj, int x, int y, const char *verb) {
 			(t->ttyp==PIT || t->ttyp==SPIKED_PIT)) {
 		/* you escaped a pit and are standing on the precipice */
 		if (Blind && flags.soundok)
-			You_hear("%s %s downwards.",
+			You_hearf("%s %s downwards.",
 				The(xname(obj)), otense(obj, "tumble"));
 		else
 			pline("%s %s into %s pit.",

src/dokick.c
@@ -387,7 +387,7 @@ void container_impact_dmg(struct obj *obj) {
 		 * but it's always exactly 1 that breaks */
 		if (otmp->otyp == EGG && otmp->spe && otmp->corpsenm >= LOW_PM)
 		    change_luck(-1);
-		You_hear("a muffled %s.", result);
+		You_hearf("a muffled %s.", result);
 		if (costly)
 		    loss += stolen_value(otmp, x, y,
 					 (boolean)shkp->mpeaceful, true, true);
@@ -1374,7 +1374,7 @@ boolean ship_object(struct obj *otmp, xchar x, xchar y, boolean shop_floor_obj) 
 		    change_luck((schar) -min(otmp->quan, 5L));
 		result = "splat";
 	    }
-	    You_hear("a muffled %s.",result);
+	    You_hearf("a muffled %s.",result);
 	    obj_extract_self(otmp);
 	    obfree(otmp, NULL);
 	    return true;

commit 68e878524b7f12470057bc8eecf2da27175500e3

Fix segfault when attacking bare-handed (originally introduced in 2d67a7c0c67f6bea71558e8cf188fcaff865279a)

src/uhitm.c
@@ -652,7 +652,7 @@ static boolean hmon_hitmon(struct monst *mon, struct obj *obj, int thrown) {
 	else if (thrown == 2) launcher = uswapwep;
 	else launcher = 0;
 
-	objenchant = !thrown && obj && obj->spe < 0 ? 0 : obj->spe;
+	objenchant = !thrown && (!obj || obj->spe < 0 ? 0 : obj->spe);
 
 	if (need_one(mon))    canhitmon = 1;
 	if (need_two(mon))    canhitmon = 2;