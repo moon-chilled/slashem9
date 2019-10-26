9872eb2fdade6515c7f864a9a3efbce849361748
Moonchild <elronnd@elronnd.net>
2019-10-25 23:46:34 -0700

    Fix MAXSPELL definition

--- include/onames.h
+++ include/onames.h
@@ -3,6 +3,6 @@
 
 #include "../src/objects.c"
 
-#define MAXSPELL ((LAST_SPELL - FIRST_SPELL) - (SPE__DUMMY_SPELL_END - SPE__DUMMY_SPELL_START))
+#define MAXSPELL ((LAST_SPELL - FIRST_SPELL) - (SPE__DUMMY_SPELL_END - SPE__DUMMY_SPELL_START) + 1)
 
 #endif //ONAMES_H
