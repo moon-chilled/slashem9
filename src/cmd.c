moon-chilled/slashem9

commit 7fd04aea489e1ecc3c3834ed3f0cd50d37c6006d

You can click on stuff to see what it is (nh)

src/cmd.c
@@ -16,7 +16,8 @@
 #define NR_OF_EOFS 20
 #endif
 
-#define CMD_TRAVEL (char)0x90
+#define CMD_TRAVEL	(char)0x90
+#define CMD_CLICKLOOK	(char)0x8F
 
 #ifdef DEBUG
 /*
@@ -95,6 +96,7 @@ static void end_of_input(void);
 #endif
 
 static const char *readchar_queue = "";
+static coord clicklook_cc;
 
 static char *parse(void);
 static bool help_dir(char, const char *);
@@ -3155,85 +3157,93 @@ void rhack(char *cmd) {
 		 * number pad. Now do not map them until here.
 		 */
 		switch (*cmd) {
-			case '5':
-				*cmd = 'g';
-				break;
-			case M('5'):
-				*cmd = 'G';
-				break;
-			case M('0'):
-				*cmd = 'I';
-				break;
+			case '5': *cmd = 'g'; break;
+			case M('5'): *cmd = 'G'; break;
+			case M('0'): *cmd = 'I'; break;
 		}
 	}
 	/* handle most movement commands */
 	do_walk = do_rush = prefix_seen = false;
 	flags.travel = iflags.travel1 = 0;
 
-	if (*cmd == DORUSH) {
-		if (movecmd(cmd[1])) {
-			flags.run = 2;
-			do_rush = true;
-		} else
-			prefix_seen = true;
-	} else if ((*cmd == '5' && iflags.num_pad) || *cmd == DORUN) {
-		if (movecmd(lowc(cmd[1]))) {
-			flags.run = 3;
-			do_rush = true;
-		} else
-			prefix_seen = true;
-	} else if ((*cmd == '-' && iflags.num_pad) || *cmd == DOFORCEFIGHT) {
+	switch (*cmd) {
+		case DORUSH: if (movecmd(cmd[1])) {
+				     flags.run = 2;
+				     do_rush = true;
+			     } else {
+				     prefix_seen = true;
+			     }
+		case '5': if (!iflags.num_pad) break; // else fallthru
+		case DORUN: if (movecmd(lowc(cmd[1]))) {
+				    flags.run = 3;
+				    do_rush = true;
+			    } else {
+				    prefix_seen = true;
+			    }
+		case '-': if (!iflags.num_pad) break; // else fallthru
 		/* Effects of movement commands and invisible monsters:
 		 * m: always move onto space (even if 'I' remembered)
 		 * F: always attack space (even if 'I' not remembered)
 		 * normal movement: attack if 'I', move otherwise
 		 */
-		if (movecmd(cmd[1])) {
-			flags.forcefight = 1;
-			do_walk = true;
-		} else
-			prefix_seen = true;
-	} else if (*cmd == DONOPICKUP) {
-		if (movecmd(cmd[1]) || u.dz) {
-			flags.run = 0;
-			flags.nopick = 1;
-			if (!u.dz)
-				do_walk = true;
-			else
-				cmd[0] = cmd[1]; /* "m<" or "m>" */
-		} else
-			prefix_seen = true;
-	} else if (*cmd == DORUN_NOPICKUP) {
-		if (movecmd(lowc(cmd[1]))) {
-			flags.run = 1;
-			flags.nopick = 1;
-			do_rush = true;
-		} else
-			prefix_seen = true;
-	} else if (*cmd == '0' && iflags.num_pad) {
-		ddoinv(); /* a convenience borrowed from the PC */
-		flags.move = false;
-		multi = 0;
-	} else {
-		if (*cmd == CMD_TRAVEL && iflags.travelcmd) {
-			flags.travel = 1;
-			iflags.travel1 = 1;
-			flags.run = 8;
-			flags.nopick = 1;
-			do_rush = true;
-		}
+		case DOFORCEFIGHT: if (movecmd(cmd[1])) {
+					   flags.forcefight = 1;
+					   do_walk = true;
+				   } else {
+					   prefix_seen = true;
+				   }
+		case DONOPICKUP: if (movecmd(cmd[1]) || u.dz) {
+					 flags.run = 0;
+					 flags.nopick = 1;
+					 if (!u.dz) {
+						 do_walk = true;
+					 } else {
+						 cmd[0] = cmd[1]; /* "m<" or "m>" */
+					 }
+				 } else {
+					 prefix_seen = true;
+				 }
 
-		if (movecmd(*cmd)) { /* ordinary movement */
-			do_walk = true;
-		} else if (movecmd(iflags.num_pad ?
-					   UNMETA(*cmd) :
-					   lowc(*cmd))) {
-			flags.run = 1;
-			do_rush = true;
-		} else if (movecmd(UNCTRL(*cmd))) {
-			flags.run = 3;
-			do_rush = true;
-		}
+		case DORUN_NOPICKUP: if (movecmd(lowc(cmd[1]))) {
+					     flags.run = 1;
+					     flags.nopick = 1;
+					     do_rush = true;
+				     } else {
+					     prefix_seen = true;
+				     }
+		case '0':
+			if (!iflags.num_pad) break;
+			ddoinv(); /* a convenience borrowed from the PC */
+			flags.move = false;
+			multi = 0;
+
+		case CMD_CLICKLOOK:
+			if (iflags.clicklook) {
+				flags.move = false;
+				do_look(2, &clicklook_cc);
+			}
+
+			return;
+		case CMD_TRAVEL:
+			if (iflags.travelcmd) {
+				flags.travel = 1;
+				iflags.travel1 = 1;
+				flags.run = 8;
+				flags.nopick = 1;
+				do_rush = true;
+				break;
+			} // else fallthru
+		default:
+			/* ordinary movement */
+			if (movecmd(*cmd)) {
+				do_walk = true;
+			} else if (movecmd(iflags.num_pad ?  UNMETA(*cmd) : lowc(*cmd))) {
+				flags.run = 1;
+				do_rush = true;
+			} else if (movecmd(UNCTRL(*cmd))) {
+				flags.run = 3;
+				do_rush = true;
+			}
 	}
 
 	/* some special prefix handling */
@@ -3532,6 +3542,13 @@ const char *click_to_cmd(int x, int y, int mod) {
 	int dir;
 	static char cmd[4];
 	cmd[1] = 0;
+
+	if (iflags.clicklook && (mod == CLICK_2)) {
+		clicklook_cc.x = x;
+		clicklook_cc.y = y;
+		cmd[0] = CMD_CLICKLOOK;
+		return cmd;
+	}
 
 	x -= u.ux;
 	y -= u.uy;

include/decl.h
@@ -234,14 +234,14 @@ extern const schar xdir[], ydir[], zdir[];
 
 extern char misc_cmds[];
 
-#define DORUSH	       misc_cmds[0]
-#define DORUN	       misc_cmds[1]
-#define DOFORCEFIGHT   misc_cmds[2]
-#define DONOPICKUP     misc_cmds[3]
-#define DORUN_NOPICKUP misc_cmds[4]
-#define DOESCAPE       misc_cmds[5]
+#define DORUSH	       'g'
+#define DORUN	       'G'
+#define DOFORCEFIGHT   'F'
+#define DONOPICKUP     'm'
+#define DORUN_NOPICKUP 'M'
+#define DOESCAPE       '\033'
 #undef DOAGAIN /* remove previous definition from config.h */
-#define DOAGAIN misc_cmds[6]
+#define DOAGAIN		'\001'
 
 /* the number of miscellaneous commands */
 #define MISC_CMD_COUNT 7

include/extern.h
@@ -1399,6 +1399,7 @@ int dowhatdoes(void);
 char *dowhatdoes_core(char, char *);
 int dohelp(void);
 int dohistory(void);
+int do_look(int mode, coord *click_cc);
 
 /* ### pcmain.c ### */
 

include/flag.h
@@ -253,6 +253,7 @@ struct instance_flags {
 	bool wc2_guicolor;	   /* allow colors in GUI (outside map) */
 
 	bool cmdassist; /* provide detailed assistance for some commands */
+	bool clicklook;
 	bool obsolete;	/* obsolete options can point at this, it isn't used */
 	/* Items which belong in flags, but are here to allow save compatibility */
 	bool lootabc;	/* use "a/b/c" rather than "o/i/b" when looting */

src/options.c
@@ -44,6 +44,7 @@ static struct Bool_Opt {
 #else
 	{"checkpoint", NULL, false, SET_IN_FILE},
 #endif
+	{"clicklook", &iflags.clicklook, true, SET_IN_GAME},
 	{"cmdassist", &iflags.cmdassist, true, SET_IN_GAME},
 #if defined(WIN32) || defined(CURSES_GRAPHICS)
 	{"color", &iflags.wc_color, true, SET_IN_GAME}, /*WC*/

src/pager.c
@@ -12,7 +12,6 @@ static boolean is_swallow_sym(int);
 static int append_str(char *, const char *);
 static struct permonst *lookat(int, int, char *, char *);
 static void checkfile(char *, struct permonst *, bool, bool);
-static int do_look(boolean);
 static boolean help_menu(int *);
 #ifdef PORT_HELP
 extern void port_help(void);
@@ -468,8 +467,10 @@ bad_data_file:
 /* also used by getpos hack in do_name.c */
 const char what_is_an_unknown_object[] = "an unknown object";
 
-/* quick => use cursor && don't search for "more info" */
-static int do_look(boolean quick) {
+int do_look(int mode, coord *click_cc) {
+	bool quick = mode == 1; // use cursor and don't search for 'more info'
+	bool clicklook = mode == 2; // right mouse-click method
+
 	char out_str[BUFSZ], look_buf[BUFSZ];
 	const char *x_str, *firstmatch = 0;
 	struct permonst *pm = 0;
@@ -484,28 +485,35 @@ static int do_look(boolean quick) {
 	int skipped_venom;    /* non-zero if we ignored "splash of venom" */
 	static const char *mon_interior = "the interior of a monster";
 
-	if (quick) {
-		from_screen = true; /* yes, we want to use the cursor */
-	} else {
-		i = ynq("Specify unknown object by cursor?");
-		if (i == 'q') return 0;
-		from_screen = (i == 'y');
-	}
-
-	if (from_screen) {
-		cc.x = u.ux;
-		cc.y = u.uy;
-		sym = 0; /* gcc -Wall lint */
-	} else {
-		getlin("Specify what? (type the word)", out_str);
-		if (out_str[0] == '\0' || out_str[0] == '\033')
-			return 0;
-
-		if (out_str[1]) { /* user typed in a complete string */
-			checkfile(out_str, pm, true, true);
-			return 0;
+	if (!clicklook) {
+		if (quick) {
+			from_screen = true; /* yes, we want to use the cursor */
+		} else {
+			i = ynq("Specify unknown object by cursor?");
+			if (i == 'q') return 0;
+			from_screen = (i == 'y');
 		}
-		sym = out_str[0];
+
+		if (from_screen) {
+			cc.x = u.ux;
+			cc.y = u.uy;
+			sym = 0; /* gcc -Wall lint */
+		} else {
+			getlin("Specify what? (type the word)", out_str);
+			if (out_str[0] == '\0' || out_str[0] == '\033')
+				return 0;
+
+			if (out_str[1]) { /* user typed in a complete string */
+				checkfile(out_str, pm, true, true);
+				return 0;
+			}
+			sym = out_str[0];
+		}
+	} else { // clicklook
+		cc.x = click_cc->x;
+		cc.y = click_cc->y;
+		sym = 0;
+		from_screen = false;
 	}
 
 	/* Save the verbose flag, we change it later. */
@@ -522,21 +530,23 @@ static int do_look(boolean quick) {
 		found = 0;
 		out_str[0] = '\0';
 
-		if (from_screen) {
+		if (from_screen || clicklook) {
 			int glyph; /* glyph at selected position */
 
-			if (flags.verbose)
-				pline("Please move the cursor to %s.",
-				      what_is_an_unknown_object);
-			else
-				pline("Pick an object.");
+			if (from_screen) {
+				if (flags.verbose)
+					pline("Please move the cursor to %s.",
+							what_is_an_unknown_object);
+				else
+					pline("Pick an object.");
 
-			ans = getpos(&cc, quick, what_is_an_unknown_object);
-			if (ans < 0 || cc.x < 0) {
-				flags.verbose = save_verbose;
-				return 0; /* done */
+				ans = getpos(&cc, quick, what_is_an_unknown_object);
+				if (ans < 0 || cc.x < 0) {
+					flags.verbose = save_verbose;
+					return 0; /* done */
+				}
+				flags.verbose = false; /* only print long question once */
 			}
-			flags.verbose = false; /* only print long question once */
 
 			/* Convert the glyph at the selected position to a symbol. */
 			glyph = glyph_at(cc.x, cc.y);
@@ -569,7 +579,7 @@ static int do_look(boolean quick) {
 
 		/* Check for monsters */
 		for (i = 0; i < MAXMCLASSES; i++) {
-			if (sym == (from_screen ? monsyms[i] : def_monsyms[i]) &&
+			if (sym == ((from_screen || clicklook) ? monsyms[i] : def_monsyms[i]) &&
 			    monexplain[i]) {
 				need_to_look = true;
 				if (!found) {
@@ -584,7 +594,7 @@ static int do_look(boolean quick) {
 		/* handle '@' as a special case if it refers to you and you're
 		   playing a character which isn't normally displayed by that
 		   symbol; firstmatch is assumed to already be set for '@' */
-		if ((from_screen ?
+		if (((from_screen || clicklook) ?
 			     (sym == monsyms[S_HUMAN] && cc.x == u.ux && cc.y == u.uy) :
 			     (sym == def_monsyms[S_HUMAN] && !iflags.showrace)) &&
 		    !(Race_if(PM_HUMAN) || Race_if(PM_ELF)) && !Upolyd)
@@ -595,7 +605,7 @@ static int do_look(boolean quick) {
 		 * and looking at something other than our own symbol, then just say
 		 * "the interior of a monster".
 		 */
-		if (u.uswallow && from_screen && is_swallow_sym(sym)) {
+		if (u.uswallow && (from_screen || clicklook) && is_swallow_sym(sym)) {
 			if (!found) {
 				sprintf(out_str, "%s       %s", utf8_tmpstr(sym), mon_interior);
 				firstmatch = mon_interior;
@@ -607,9 +617,9 @@ static int do_look(boolean quick) {
 
 		/* Now check for objects */
 		for (i = 1; i < MAXOCLASSES; i++) {
-			if (sym == (from_screen ? oc_syms[i] : def_oc_syms[i])) {
+			if (sym == ((from_screen || clicklook) ? oc_syms[i] : def_oc_syms[i])) {
 				need_to_look = true;
-				if (from_screen && i == VENOM_CLASS) {
+				if ((from_screen || clicklook) && i == VENOM_CLASS) {
 					skipped_venom++;
 					continue;
 				}
@@ -639,7 +649,7 @@ static int do_look(boolean quick) {
 		/* Now check for graphics symbols */
 		for (hit_trap = false, i = 0; i < MAXPCHARS; i++) {
 			x_str = sym_desc[i].explanation;
-			if (sym == (from_screen ? showsyms[i] : ascii_graphics[i]) && *x_str) {
+			if (sym == ((from_screen || clicklook) ? showsyms[i] : ascii_graphics[i]) && *x_str) {
 				/* avoid "an air", "a water", "a floor of a room", or "a dark part of a room" */
 				int article = ((i == S_room) || (i == S_darkroom)) ? 2 :  // 2=>"the"
 						      !(strcmp(x_str, "air") == 0 ||	  /* 1=>"an"  */
@@ -677,7 +687,7 @@ static int do_look(boolean quick) {
 		/* Now check for warning symbols */
 		for (i = 1; i < MAXWARNINGS; i++) {
 			x_str = sym_desc[S_warn0 + i].explanation;
-			if (sym == (from_screen ? showsyms : ascii_graphics)[S_warn0 + i]) {
+			if (sym == ((from_screen || clicklook) ? showsyms : ascii_graphics)[S_warn0 + i]) {
 				if (!found) {
 					sprintf(out_str, "%s       %s", utf8_tmpstr(sym), sym_desc[S_warn0 + i].explanation);
 					firstmatch = sym_desc[S_warn0 + i].explanation;
@@ -687,7 +697,7 @@ static int do_look(boolean quick) {
 				}
 				/* Kludge: warning trumps boulders on the display.
 				   Reveal the boulder too or player can get confused */
-				if (from_screen && sobj_at(BOULDER, cc.x, cc.y))
+				if ((from_screen || clicklook) && sobj_at(BOULDER, cc.x, cc.y))
 					strcat(out_str, " co-located with a boulder");
 				break; /* out of for loop*/
 			}
@@ -709,7 +719,7 @@ static int do_look(boolean quick) {
 		 * If we are looking at the screen, follow multiple possibilities or
 		 * an ambiguous explanation by something more detailed.
 		 */
-		if (from_screen) {
+		if (from_screen || clicklook) {
 			if (found > 1 || need_to_look) {
 				char monbuf[QBUFSZ];
 				char temp_buf[BUFSZ];
@@ -741,7 +751,7 @@ static int do_look(boolean quick) {
 			pline("%s", out_str);
 			/* check the data file for information about this thing */
 			if (found == 1 && ans != LOOK_QUICK && ans != LOOK_ONCE &&
-			    (ans == LOOK_VERBOSE || (flags.help && !quick))) {
+			    (ans == LOOK_VERBOSE || (flags.help && !quick)) && !clicklook) {
 				char temp_buf[BUFSZ];
 				strcpy(temp_buf, level.flags.lethe && !strcmp(firstmatch, "water") ?
 							 "lethe" :
@@ -752,18 +762,18 @@ static int do_look(boolean quick) {
 			pline("I've never heard of such things.");
 		}
 
-	} while (from_screen && !quick && ans != LOOK_ONCE);
+	} while (from_screen && !quick && ans != LOOK_ONCE && !clicklook);
 
 	flags.verbose = save_verbose;
 	return 0;
 }
 
 int dowhatis(void) {
-	return do_look(false);
+	return do_look(0, NULL);
 }
 
 int doquickwhatis(void) {
-	return do_look(true);
+	return do_look(1, NULL);
 }
 
 int doidtrap(void) {

commit f20a00fb9f042be29c9e61e13a1582bd02591df6

Fix forcefight (and possibly others).  Breakage introduced in 7fd04aea489e1ecc3c3834ed3f0cd50d37c6006d

src/cmd.c
@@ -3171,63 +3171,78 @@ void rhack(char *cmd) {
 	context.travel = context.travel1 = 0;
 
 	switch (*cmd) {
-		case DORUSH: if (movecmd(cmd[1])) {
-				     context.run = 2;
-				     do_rush = true;
-			     } else {
-				     prefix_seen = true;
-			     }
+		case DORUSH:
+			if (movecmd(cmd[1])) {
+				context.run = 2;
+				do_rush = true;
+			} else {
+				prefix_seen = true;
+			}
+			break;
+
 		case '5': if (!iflags.num_pad) break; // else fallthru
-		case DORUN: if (movecmd(lowc(cmd[1]))) {
+		case DORUN:
+			if (movecmd(lowc(cmd[1]))) {
 				    context.run = 3;
 				    do_rush = true;
-			    } else {
-				    prefix_seen = true;
-			    }
+			} else {
+				prefix_seen = true;
+			}
+			break;
+
 		case '-': if (!iflags.num_pad) break; // else fallthru
 		/* Effects of movement commands and invisible monsters:
 		 * m: always move onto space (even if 'I' remembered)
 		 * F: always attack space (even if 'I' not remembered)
 		 * normal movement: attack if 'I', move otherwise
 		 */
-		case DOFORCEFIGHT: if (movecmd(cmd[1])) {
-					   context.forcefight = 1;
-					   do_walk = true;
-				   } else {
-					   prefix_seen = true;
-				   }
-		case DONOPICKUP: if (movecmd(cmd[1]) || u.dz) {
-					 context.run = 0;
-					 context.nopick = 1;
-					 if (!u.dz) {
-						 do_walk = true;
-					 } else {
-						 cmd[0] = cmd[1]; /* "m<" or "m>" */
-					 }
-				 } else {
-					 prefix_seen = true;
-				 }
+		case DOFORCEFIGHT:
+			if (movecmd(cmd[1])) {
+				context.forcefight = 1;
+				do_walk = true;
+			} else {
+				prefix_seen = true;
+			}
+			break;
 
-		case DORUN_NOPICKUP: if (movecmd(lowc(cmd[1]))) {
-					     context.run = 1;
-					     context.nopick = 1;
-					     do_rush = true;
-				     } else {
-					     prefix_seen = true;
-				     }
+		case DONOPICKUP:
+			if (movecmd(cmd[1]) || u.dz) {
+				context.run = 0;
+				context.nopick = 1;
+				if (!u.dz) {
+					do_walk = true;
+				} else {
+					cmd[0] = cmd[1]; /* "m<" or "m>" */
+				}
+			} else {
+				prefix_seen = true;
+			}
+			break;
+
+		case DORUN_NOPICKUP:
+			if (movecmd(lowc(cmd[1]))) {
+				context.run = 1;
+				context.nopick = 1;
+				do_rush = true;
+			} else {
+				prefix_seen = true;
+			}
+			break;
+
 		case '0':
 			if (!iflags.num_pad) break;
 			ddoinv(); /* a convenience borrowed from the PC */
 			context.move = false;
 			multi = 0;
+			break;
 
 		case CMD_CLICKLOOK:
 			if (iflags.clicklook) {
 				context.move = false;
 				do_look(2, &clicklook_cc);
 			}
-
 			return;
+
 		case CMD_TRAVEL:
 			if (iflags.travelcmd) {
 				context.travel = 1;